b04da2f5bf310218154b5f08486952f9de046f7b
Dmitrii Solomin <ssdmitrii28@gmail.com>
2023-01-24 08:29:32 +0700

    add files

---
+++ 931921.solomin.dmitrii.lab12/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _931921.solomin.dmitrii.lab12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
